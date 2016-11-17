d24e74dfa73fd044e2a94bea5d714eae6464eacf
David Thomas <m8pple@github.com>
2016-11-17 07:23:05 +0000

    Doc->docs (properly)

---
+++ docs/heat/render_svg.js
@@ -0,0 +1,11 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var RenderSVG = (function () {
+        function RenderSVG() {
+        }
+        return RenderSVG;
+    }());
+    exports.RenderSVG = RenderSVG;
+    ;
+});
+//# sourceMappingURL=render_svg.js.map
