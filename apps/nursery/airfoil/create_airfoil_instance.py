ad430f03d8d33dc1c9f556d66272240ada73ecc5
David Thomas <m8pple@github.com>
2017-08-12 13:15:33 +0100

    Instance now contains positional data.

--- apps/nursery/airfoil/create_airfoil_instance.py
+++ apps/nursery/airfoil/create_airfoil_instance.py
@@ -5,6 +5,8 @@ from graph.core import *
 from graph.load_xml import load_graph_types_and_instances
 from graph.save_xml_stream import save_graph
 
+from graph.metadata import create_device_instance_key
+
 import h5py
 import math
 import sys
@@ -31,13 +33,73 @@ def render_model(m):
         "mach":m["globals"].mach,
         "alpha":m["globals"].alpha,
         "qinf":m["globals"].qinf,
-    })
+        }
+    )
+
+
+    ###################################################################
+    ###################################################################
+    ##
+    ## Create all devices
     
-    nodes=[ graph.create_device_instance("n{}".format(n.id),nodeType,{"x":n.x,"fanout":0}) for n in m["nodes"] ]
-    cells=[ graph.create_device_instance("c{}".format(c.id),cellType,{"id":c.id,"qinit":c.q}) for c in m["cells"] ]
-    edges=[ graph.create_device_instance("e{}".format(e.id),edgeType,{"id":e.id}) for e in m["edges"] ]
-    bedges=[ graph.create_device_instance("be{}".format(be.id),bedgeType,{"bound":int(be.bound),"id":be.id}) for be in m["bedges"] ] 
+    o_cells=m["cells"]
+    o_nodes=m["nodes"]
+    o_pcell=m["pcell"]
+    o_pedge=m["pedge"]
+    o_pbedge=m["pbedge"]
+    
+    nodes=[
+        graph.create_device_instance("n{}".format(n.id),nodeType,
+            {"x":n.x,"fanout":0},
+            {"loc":[n.x[0],n.x[1]]}
+        )
+        for n in m["nodes"]
+    ]
+    
+    def calc_hull(id,map):
+        nodes=[o_nodes[i] for i in map[id]]
+        return [ node.x for node in nodes ]
+    
+    def calc_loc(hull):
+        (xx,yy)=zip(*hull) # Convert from list of pairs to pair of lists
+        return [ sum(xx)/len(xx) , sum(yy)/len(yy) ]
+    
+    def calc_spatial_props(id,map):
+        hull=calc_hull(id,map)
+        loc=calc_loc(hull)
+        return {"hull":hull,"loc":loc}
+    
+    cells=[
+        graph.create_device_instance("c{}".format(c.id),cellType,
+            {"id":c.id,"qinit":c.q},
+            calc_spatial_props(c.id,o_pcell)
+        )
+        for c in m["cells"]
+    ]
+    
+    edges=[
+        graph.create_device_instance("e{}".format(e.id),edgeType,
+            {"id":e.id},
+            calc_spatial_props(e.id,o_pedge)
+        )
+        for e in m["edges"]
+    ]
+   
+    bedges=[
+        graph.create_device_instance("be{}".format(be.id),bedgeType,
+            {"bound":int(be.bound),"id":be.id},
+            calc_spatial_props(be.id,o_pbedge)
+        )
+        for be in m["bedges"]
+    ] 
+    
     printer=graph.create_device_instance("print",printerType,{"fanin":len(cells),"delta_print":100,"delta_exit":1000})
+    
+    
+    ###################################################################
+    ###################################################################
+    ##
+    ## Add all edges
     
     cell_edges=m["cell_edges"]
        
@@ -84,15 +146,22 @@ def render_model(m):
     
 
 if __name__=="__main__":
-    import mock
-    
+    import mock    
+    import argparse
 
-    if True:
-        src=h5py.File(appBase+"/new_grid.h5")
-        model=mock.load(src)
-    else:
-        model=mock.create_square(3)
+    parser = argparse.ArgumentParser(description='Generate graph for airfoil.')
+    parser.add_argument('source', type=str, help='Input hdf5 mesh, or take new_grid.h5 by default', nargs="?", default=appBase+"/new_grid.h5")
+    parser.add_argument('-o', dest='output', default="-", help='Where to save the file (- for stdout)')
+    args = parser.parse_args()
+    
+    src=h5py.File(args.source)
+    model=mock.load(src)
+    
+    #model=mock.create_square(3)
     
     graph=render_model(model)
     
-    save_graph(graph,sys.stdout)        
+    dst=sys.stdout
+    if args.output!="-":
+        dst=args.output
+    save_graph(graph,dst)
