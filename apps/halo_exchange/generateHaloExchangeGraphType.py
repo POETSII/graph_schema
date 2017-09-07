34d066b6cae0b92e889fc5366efabddc6936e6ae
STFleming <sf306@ic.ac.uk>
2017-09-07 15:30:21 +0100

    update code fragments for the parametrisable agglomerated nodes are now specified separately to the rest of the python used to generate the xml graph type. I have included a simple example where each node just accumulates the values of its neighbours, in this example there are 4 boundary devices surrounding a 9x9 center device. Tested in epoch_sim but not yet in hardware. I'm moving onto refining the interface now.

--- apps/halo_exchange/generateHaloExchangeGraphType.py
+++ apps/halo_exchange/generateHaloExchangeGraphType.py
@@ -41,30 +41,96 @@ def main(argv):
 	xmlo+= '\n'
 	xmlo+= '\t<DeviceTypes>\n'
 
-	c_timestep = '\n'
-	c_timestep += '	uint32_t hL = gp->haloLength;\n'
-	c_timestep += '	uint32_t nV[hL*hL];\n' 
-	c_timestep += '	//resolve the four corners\n' 
-	c_timestep += '	nV[0] = cN[0] + cW[0] + state->v[1] + state->v[hL]; //NW corner\n' 
-	c_timestep += '	nV[hL-1] = cN[hL-1] + cE[0] + state->v[hL-2] + state->v[hL + (hL-1)]; //NE corner\n' 
-	c_timestep += '	for(int i=0; i<hL; i++) {\n'
-	c_timestep += '		state->v[i] = state->cN[i];\n'
-	c_timestep += '	}\n'
-	c_timestep += '	//resolve the east face\n' 
-	c_timestep += '	for(int i=1; i<hL; i++) {\n'
-	c_timestep += '		state->v[((i+1)*hL)-1] = state->cE[i];\n'
-	c_timestep += '		state->v[(hL * (hL - 1)) + i] = state->cS[i];\n'
-	c_timestep += '		state->v[i*hL] = state->cW[i];\n'
-	c_timestep += '	}\n'
-	c_timestep += '	//Update the new state\n'
-	c_timestep += '	for(int i=0; i<hL*hL; i++) {\n'
-	c_timestep += '		state->v[i] = nV[i];\n'
-	c_timestep += '	}\n'
-	
-	b_timestep = '\n'
+	tabs = '\t\t\t\t'
 
-	xmlo+= generate_NbyN_Cluster(N, debug)
-	xmlo+= generate_lengthN_boundary(N, debug)
+	# Example of a 4x4 FE agglomerated node
+	#	hL is the length of the halo exchange (i.e. the length of the grid)
+	#
+	# 				N(0)		N(1)		N(2)		N(3) 
+	# W(0)		0				1				2				3		E(0)	
+	# W(1)		4				5				6				7		E(1)
+	# W(2)		8				9				10			11	E(2)
+	# W(3)		12			13			14			15	E(3)
+	#					S(0)		S(1)		S(2)		S(3)
+
+	# if ( i < hL ) we are at a north facing edge 
+  # if ( i % (hL-1) == 0 ) we are at an east facing edge 
+	# if ( i >= (hL*hL -1) - (hL - 1) ) we are at a south facing edge 
+	# if ( i % hL == 0 ) we are at a west facing edge 
+
+	c_timestep =  tabs + ' uint32_t hL = gp->haloLength;\n'
+	c_timestep += tabs + ' uint32_t tmpV[hL*hL] = { 0 };\n'
+	c_timestep += tabs + ' for(uint32_t i=0; i<hL*hL; i++) {\n'
+	c_timestep += tabs + ' 	//Accumulate all points surrounding the current point\n'
+	c_timestep += tabs + ' \n'
+	c_timestep += tabs + ' 	//North of point\n'
+	c_timestep += tabs + ' 	if(i<hL) //We are at the northern face of this node and need to use the halo value	\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->cN[i];\n'
+	c_timestep += tabs + ' 	else\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->v[i-hL];\n'
+	c_timestep += tabs + ' \n'
+	c_timestep += tabs + ' 	//East of point\n'
+	c_timestep += tabs + ' 	if((i!=0) && (i % hL == (hL-1))) //We are at the eastern face of this node and need to use the halo value\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->cE[i/(hL-1)]; \n'
+	c_timestep += tabs + ' 	else\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->v[i+1];\n'
+	c_timestep += tabs + ' \n'
+	c_timestep += tabs + ' 	//South of point\n'
+	c_timestep += tabs + ' 	if(i >= (hL*hL -1) - (hL-1)) //We are at the southern face of this node and need to use the halo value\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->cS[i - ((hL*hL -1) - (hL-1))]; \n'
+	c_timestep += tabs + ' 	else\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->v[i+hL];\n'
+	c_timestep += tabs + ' \n'
+	c_timestep += tabs + ' 	//West of point\n'
+	c_timestep += tabs + ' 	if(i % hL == 0) //We are at the western face of this node and need to use the halo value\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->cW[i/hL];\n'
+	c_timestep += tabs + ' 	else\n'
+	c_timestep += tabs + ' 		tmpV[i] += state->v[i-1];\n'
+	c_timestep += tabs + ' }\n'
+	c_timestep += tabs + ' \n'
+	c_timestep += tabs + ' for(uint32_t i=0; i<hL*hL; i++) {\n'
+	c_timestep += tabs + ' 	state->v[i] = tmpV[i];\n'
+	c_timestep += tabs + ' }\n'
+	c_timestep += tabs + ' for(uint32_t i=0; i<hL; i++){' 
+	c_timestep += tabs + '     handler_log(2 ,"%u\t\t%u\t\t%u\t\t%u\t\t%u\t\t%u\t\t%u\t\t%u\t\t%u", tmpV[(i*hL)+0],tmpV[(i*hL)+1],tmpV[(i*hL)+2],tmpV[(i*hL)+3],tmpV[(i*hL)+4],tmpV[(i*hL)+5],tmpV[(i*hL)+6],tmpV[(i*hL)+7],tmpV[(i*hL)+8]);\n' 
+	c_timestep += tabs + ' }\n' 
+	c_timestep += tabs + ' return 0;\n'
+
+	
+	# Boundary update function body, example boundary_4
+	#
+	# 0	- cV[0]
+	#	1 - cV[1]
+	#	2 - cV[2]
+	#	3 - cV[3]
+	#
+	# Currently it ignores it's own boundary and does nothing special, just accumulates it's neighbours together where it can
+	# Boundary only has N, E, S -- the west edge is assumed to be the boundary. In reality this might be rotated and not truely be the western face..
+
+	b_timestep =  tabs + '\n'
+	b_timestep += tabs + 'uint32_t hL = gp->haloLength;\n'
+	b_timestep += tabs + 'uint32_t tmpV[hL];	\n'
+	b_timestep += tabs + '\n'
+	b_timestep += tabs + '//c = NorthOf(c) + EastOf(c) + SouthOf(c) \n'
+	b_timestep += tabs + '//Northmost element \n'
+	b_timestep += tabs + 'tmpV[0] = 0 + state->cV[0] + state->v[1]; \n'
+	b_timestep += tabs + '\n'
+	b_timestep += tabs + '//Middle Elements \n'
+	b_timestep += tabs + 'for(uint32_t i=1; i<hL-1; i++) { \n'
+	b_timestep += tabs + '\ttmpV[i] = state->v[i-1] + state->cV[i] + state->v[i+1]; \n'
+	b_timestep += tabs + '} \n'
+	b_timestep += tabs + '\n'
+	b_timestep += tabs + '//Southmost element \n'
+	b_timestep += tabs + 'tmpV[hL-1] = state->v[hL-2] + state->cV[hL-1] + 0; \n'
+	b_timestep += tabs + '\n'
+	b_timestep += tabs + '//Load tmp value array into the real value array \n'
+	b_timestep += tabs + 'for(uint32_t i=0; i<hL; i++) { \n'
+	b_timestep += tabs + '\tstate->v[i] = tmpV[i]; \n'
+	b_timestep += tabs + '} \n'
+	b_timestep += tabs + 'return 1;\n'
+
+	xmlo+= generate_NbyN_Cluster(N, debug, c_timestep)
+	xmlo+= generate_lengthN_boundary(N, debug, b_timestep)
 	xmlo+= generate_exitNode(debug)
 
 	xmlo+= '\t</DeviceTypes>\n'
@@ -131,6 +197,7 @@ def generate_lengthN_boundary(N,debug, computeTimestep):
 	o+='\t\t<State>\n'
 	o+='\t\t\t<Array type=\"uint32_t\" name=\"cV\" length=\"'+str(N)+'\"/>\n'
 	o+='\t\t\t<Array type=\"uint32_t\" name=\"nV\" length=\"'+str(N)+'\"/>\n'
+	o+='\t\t\t<Array type=\"uint32_t\" name=\"v\" length=\"'+str(N)+'\"/>\n'
 	o+='\t\t\t<Scalar type=\"uint32_t\" name=\"t\"/>\n'
 	o+='\t\t\t<Scalar type=\"int8_t\" name=\"cAvail\"/>\n'
 	o+='\t\t\t<Scalar type=\"int8_t\" name=\"nAvail\"/>\n'
@@ -159,6 +226,7 @@ def generate_lengthN_boundary(N,debug, computeTimestep):
 	o+='\t\t\t\tfor(uint32_t i=0; i<graphProperties->haloLength; i++){\n'
 	o+='\t\t\t\t\tdeviceState->cV[i] = 0;\n'
 	o+='\t\t\t\t\tdeviceState->nV[i] = 0;\n'
+	o+='\t\t\t\t\tdeviceState->v[i] = 0;\n'
 	o+='\t\t\t\t}\n'
 	o+='\t\t\t]]></OnReceive>\n'
 	o+='\t\t</InputPin>\n'
@@ -166,8 +234,9 @@ def generate_lengthN_boundary(N,debug, computeTimestep):
 
 	o+='\t\t<SharedCode><![CDATA[\n'
 	o+='\t\t\tenum orientation_t { north, east, south, west };\n'
-	o+='\t\t\tuint32_t computeTimestep(boundary_9_state_t *state, const halo_exchange_properties_t *gp) {\n'
+	o+='\t\t\tuint32_t computeTimestep(boundary_9_state_t *state, const halo_exchange_properties_t *gp, OrchestratorServices *orchestrator) {\n'
 	o+='\t\t\t\t//Process the timestep for this device\n'
+	o+='\t\t\t\tHandlerLogImpl handler_log(orchestrator);\n'
 	o+= computeTimestep #Inject the input code for how the timestep should be computed
 	o+='\t\t\t}\n'
 	o+='\t\t]]></SharedCode>\n'
@@ -177,7 +246,7 @@ def generate_lengthN_boundary(N,debug, computeTimestep):
 	o+='\t\t\t<OnSend><![CDATA[\n'
 	o+='\t\t\t\tuint32_t hL = graphProperties->haloLength;\n'
 	o+='\t\t\t\tfor(uint32_t i=0; i< hL; i++) {\n'
-	o+='\t\t\t\t\tmessage->v[i] = deviceState->cV[i];\n'
+	o+='\t\t\t\t\tmessage->v[i] = deviceState->v[i];\n'
 	o+='\t\t\t\t}\n'
 	o+='\t\t\t\tif(deviceState->cAvail) {\n'
 	o+='\t\t\t\t\tdeviceState->t = deviceState->t + graphProperties->dt;\n'
@@ -210,9 +279,7 @@ def generate_lengthN_boundary(N,debug, computeTimestep):
 	o+='\t\t\t\tuint32_t hL = graphProperties->haloLength;\n'
 	o+='\t\t\t\tif(message->t == deviceState->t) {\n'
 	o+='\t\t\t\t\tdeviceState->cAvail = 1;\n'
-	o+='\t\t\t\t\tfor(uint32_t i=0; i<hL; i++) {\n'
-	o+='\t\t\t\t\t\tdeviceState->cV[i] = message->v[i];\n'
-	o+='\t\t\t\t\t}\n'
+	o+='\t\t\t\t\tcomputeTimestep(deviceState, graphProperties, orchestrator);\n'
 	o+='\t\t\t\t} else if (message->t == deviceState->t+hL) {\n'
 	o+='\t\t\t\t\tdeviceState->nAvail = 1;\n'
 	o+='\t\t\t\t\tfor(uint32_t i=0; i<hL; i++) {\n'
@@ -249,11 +316,12 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 	o+='\t\t\t<Array type=\"uint8_t\" name=\"c_arrivalFlags\" length=\"4\"/>\n'
 	o+='\t\t\t<Array type=\"uint8_t\" name=\"n_arrivalFlags\" length=\"4\"/>\n'
 	o+='\t\t\t<Array type=\"uint8_t\" name=\"sentFlags\" length=\"4\"/>\n'
+	o+='\t\t\t<Scalar type=\"uint8_t\" name=\"processed\"/>\n'
 	o+='\t\t</State>\n'
 	o+='\n'
 	o+='\t\t<ReadyToSend><![CDATA[\n'
 	o+='\t\t\t*readyToSend=0;\n'
-	o+='\t\t\tif(allArrived(deviceState, graphProperties)) {\n'
+	o+='\t\t\tif(deviceState->processed) {\n'
 	o+='\t\t\t\tif(!deviceState->sentFlags[north])\n'
 	o+='\t\t\t\t\t*readyToSend = *readyToSend | RTS_FLAG_north_out;\n'
 	o+='\t\t\t\tif(!deviceState->sentFlags[east])\n'
@@ -279,6 +347,7 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 	if debug:
 		o+='\t\t\t\thandler_log(2, \"center device is being initialised\");\n'
 	o+='\t\t\t\tdeviceState->t=0;\n'
+	o+='\t\t\t\tdeviceState->processed=0;\n'
 	o+='\t\t\t\tfor(uint32_t i=0; i<graphProperties->nodesPerDevice; i++) {\n'
 	o+='\t\t\t\t\tdeviceState->v[i] = 0;\n'
 	o+='\t\t\t\t}\n'
@@ -294,8 +363,9 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 	o+='\t\t<SharedCode><![CDATA[\n'
 	o+='\t\t\tenum orientation_t { north, east, south, west };\n'
 	o+='\n'
-	o+='\t\t\tuint32_t computeTimestep(cell_'+str(N)+'by'+str(N)+'_state_t *state, const halo_exchange_properties_t *gp) {\n'
+	o+='\t\t\tuint32_t computeTimestep(cell_'+str(N)+'by'+str(N)+'_state_t *state, const halo_exchange_properties_t *gp, OrchestratorServices *orchestrator) {\n'
 	o+='\t\t\t\t //Process the timestep for this device\n'
+	o+='\t\t\t\t HandlerLogImpl handler_log(orchestrator);\n\n'
 	o+= computeTimestep #inject the code for computing each timestep
 	o+='\t\t\t}\n'
 	o+='\n'
@@ -305,6 +375,12 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 	o+='\t\t\t\t\t\treturn false;\n'
 	o+='\t\t\t\t\t}\n'
 	o+='\t\t\t\treturn true;\n'
+	o+='\t\t\t}\n'
+	o+='\t\t\tvoid clearArrived(cell_'+str(N)+'by'+str(N)+'_state_t *state) {\n'
+	o+='\t\t\t\tfor(uint32_t i=0; i<4; i++) {\n'
+	o+='\t\t\t\t\t\tstate->c_arrivalFlags[i] = 0;\n'
+	o+='\t\t\t\t\t}\n'
+	o+='\t\t\t\treturn;\n'
 	o+='\t\t\t}\n'
 	o+='\n'
 	o+='\t\t\tbool allSent(const cell_'+str(N)+'by'+str(N)+'_state_t *state, const halo_exchange_properties_t *gp) {\n'
@@ -329,6 +405,7 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 	o+='\t\t\t\t\tstate->cW[i] = state->nW[i];\n'
 	o+='\t\t\t}\n'
 	o+='\t\t\tstate->t = state->t + gp->dt;\n'
+	o+='\t\t\tstate->processed = 0;\n'
 	o+='\t\t\treturn;\n'
 	o+='\t\t}\n'
 	o+='\n'
@@ -375,7 +452,9 @@ def generate_NbyN_Cluster(N, debug, computeTimestep):
 			o+='\t\t\t\thandler_log(2, "\tcurrent arrivals: n:%d e:%d s:%d w%d", deviceState->c_arrivalFlags[north], deviceState->c_arrivalFlags[east],deviceState->c_arrivalFlags[south],deviceState->c_arrivalFlags[west]);\n'
 		o+='\t\t\t\tif(message->t == deviceState->t) {\n'
 		o+='\t\t\t\t\tif(allArrived(deviceState, graphProperties)) {\n'
-		o+='\t\t\t\t\t\tcomputeTimestep(deviceState, graphProperties);\n'
+		o+='\t\t\t\t\t\tdeviceState->processed=1;;\n'
+		o+='\t\t\t\t\t\tclearArrived(deviceState);\n'
+		o+='\t\t\t\t\t\tcomputeTimestep(deviceState, graphProperties, orchestrator);\n'
 		if debug:
 			o+='\t\t\t\t\t\thandler_log(2, "data-transfer complete computing timestep.");\n'
 		o+='\t\t\t\t\t}\n'
